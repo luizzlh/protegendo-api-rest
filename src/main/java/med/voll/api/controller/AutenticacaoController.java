35b3e68dea44fa63a2234a7ae6abc3b8cb96c230
Luiz <luizzlh2004@gmail.com>
2026-01-18 14:46:45 -0300

    Adicionando dependencia do JWT Auth0 para Json Web Token.

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -23,7 +23,9 @@ public class AutenticacaoController {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok();
     }
+
+
 
 }
